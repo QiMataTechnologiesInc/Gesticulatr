0774890e67ec0eb61ed51ed5bb49a2317f78bf0f
Ian Philpot <ian.philpot@gmail.com>
2015-08-30 12:17:15 -0400

    Updated web app

---
+++ WebApp/WebApp/BandHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApp
+{
+    public class BandHub : Hub
+    {
+        public void UpdateData(double x, double y, double z)
+        {
+            var _x = x;
+            var _y = y;
+            var _z = z;
+
+            Clients.All.sendData(x, y, z);
+        }
+    }
+}
